021e0b18186a7f1a24e1ff1ba2efe2ab4ee83965
Arigela sriraghu <sriraghu.arigela@improve10x.com>
2023-03-14 16:05:25 +0530

    create new Activity FoodDeliveryActivity and create SplashActivity and create xml design

---
+++ app/src/main/java/com/improve10x/fooddelivery/FoodDeliveryActivity.java
@@ -0,0 +1,14 @@
+package com.improve10x.fooddelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodDeliveryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_delivery);
+    }
+}
